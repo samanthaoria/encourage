d019acab7fc64af8a4a875504784ed5678d84c53
Samantha Lima Luna Oriá <samanthalimaadv@yahoo.com.br>
2020-11-12 12:15:05 +0100

    message edited

--- message.js
+++ message.js
@@ -1,3 +1,3 @@
 const message =
-  " We know that the course is very intense, but let's do it together, maybe it gets more easier to all of us!";
+  " We know that the course is very intense, but let's do it together, maybe it gets easier to all of us!";
 console.log(message);
